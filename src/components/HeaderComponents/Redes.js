ce7289d1dc79eacf924f29140a32b2ce595a2e95
Santiago Alarcon <santiagoalarcon2@hotmail.com>
2019-10-10 10:51:18 -0300

    add Header component

---
+++ src/components/HeaderComponents/Redes.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export default class Redes extends Component {
+    render(){
+        return  (                
+            <ul class="redes animated fadeIn">
+                <li>
+                    <a href="" target="_blank">
+                    <img class="icono-redes" src="./img/whatsapp.png" alt=""/>
+                    </a>
+                </li>
+                <li>
+                    <a href="" target="_blank">
+                    <img class="icono-redes" src="./img/facebook.png" alt=""/>
+                    </a>
+                </li>
+                <li>
+                    <a href="" target="_blank">
+                    <img class="icono-redes" src="./img/instagram.png" alt=""/>
+                    </a>
+                </li>                                   
+             </ul>
+        );
+                     
+                       
+    }
+}
